7ccbee3494ecfed67623beeed8a7342e63c96459
Cauê <cauemateus@hotmail.com>
2017-11-12 23:14:27 -0200

    Changed some function names

--- server/controller/ProductsController.js
+++ server/controller/ProductsController.js
@@ -2,32 +2,32 @@ var ProductModel = require("../model/ProductModel");
 //var request = require('request'); Use this to call other services-- External Rest services
 
 // This defines my Rest Service
-var productsController = function (app) {
+var productsController = function(app) {
 
-    app.post("/lab_banco/sales", saveSale);    
-    app.get("/lab_banco/sales",fetchAllCallback);    
-    app.get("/lab_banco/sales/:saleid",fetchOneCallback); 
-    
+    app.post("/lab_banco/sales", saveSale);
+    app.get("/lab_banco/sales", fetchAllSales);
+    app.get("/lab_banco/sales/:saleid", fetchOneSale);
+
     //Save Sale Callback
     function saveSale(req, res) {
         console.log("Salvar venda");
-        ProductModel.save(req.body, function (error, result, jsonData) {
+        ProductModel.save(req.body, function(error, result, jsonData) {
             if (error) {
                 res.set("lab_banco_msg", "Nao foi possivel salvar os dados");
                 res.set("lab_banco_msg", "Venda adicionada com sucesso!");
-                res.status(400).send();                
+                res.status(400).send();
             }
             console.log(JSON.stringify(jsonData));
             res.set("lab_banco_msg", "Venda adicionada com sucesso!");
             res.status(201).send(jsonData);
         });
     }
-    
+
     // Fetch by ID Callback
-    function fetchOneCallback(req,res){
-        ProductModel.fetchOne(req.params.saleid,function(error,result){
+    function fetchOneSale(req, res) {
+        ProductModel.fetchOne(req.params.saleid, function(error, result) {
             if (error) {
-                res.set("lab_banco_msg",error.message)
+                res.set("lab_banco_msg", error.message)
                 res.status(400).send();
                 return;
             }
@@ -36,10 +36,10 @@ var productsController = function (app) {
     }
 
     // Fetch All Callback
-    function fetchAllCallback(req,res){
-        ProductModel.fetchAll(req.params.saleid,function(error,result){
+    function fetchAllSales(req, res) {
+        ProductModel.fetchAll(req.params.saleid, function(error, result) {
             if (error) {
-                res.set("lab_banco_msg",error.message)
+                res.set("lab_banco_msg", error.message)
                 res.status(400).send();
                 return;
             }
